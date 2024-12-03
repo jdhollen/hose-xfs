4210fce61e760abf1d0f4f20094eb57fa9c25d31
Jim Hollenbach <jim@buildbuddy.io>
2024-12-03 08:29:21 -0800

    bigger raid, more agcount

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ set -o errexit -o nounset -o pipefail -x
 
 sudo mkdir /var/buildbuddy
 
-sudo mdadm --create --verbose /dev/md103 --level=0 --raid-devices=2 /dev/nvme3n1 /dev/nvme4n1
+sudo mdadm --create --verbose /dev/md103 --level=0 --raid-devices=4 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 /dev/nvme4n1
 
-sudo mkfs.xfs /dev/md103 -l size=2038m -f
+sudo mkfs.xfs /dev/md103 -l size=2038m -d agcount=112 -f
 
 sudo mount -o logbsize=256k /dev/md103 /var/buildbuddy
 
