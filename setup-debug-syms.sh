71b4dc032e78302fe8359e095d21f86769414ef1
Jim Hollenbach <jim@buildbuddy.io>
2024-12-03 12:07:19 -0800

    message

--- setup-debug-syms.sh
+++ setup-debug-syms.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail -x
 
+# i think you don't need this script at all on 22.04 if you're just running a vanilla kernel?
+
 sudo apt install ubuntu-dbgsym-keyring
 
 echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe multiverse
