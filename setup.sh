ee785ae2027e25c0a10da262a25263606dc8f010
Jim Hollenbach <jim@buildbuddy.io>
2024-12-02 18:33:01 -0800

    force xfs creation

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo mkdir /var/buildbuddy
 
 sudo mdadm --create --verbose /dev/md103 --level=0 --raid-devices=2 /dev/nvme3n1 /dev/nvme4n1
 
-sudo mkfs.xfs /dev/md103 -l size=2038m
+sudo mkfs.xfs /dev/md103 -l size=2038m -f
 
 sudo mount -o logbsize=256k /dev/md103 /var/buildbuddy
 
