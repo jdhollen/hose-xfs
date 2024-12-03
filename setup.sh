1dc6456c33946d4646da3c3ee590dbe64640816b
Jim Hollenbach <jim@buildbuddy.io>
2024-12-02 18:20:54 -0800

    lower ag count

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo mkdir /var/buildbuddy
 
 sudo mdadm --create --verbose /dev/md103 --level=0 --raid-devices=2 /dev/nvme3n1 /dev/nvme4n1
 
-sudo mkfs.xfs /dev/md103 -d agcount=512 -l size=2038m
+sudo mkfs.xfs /dev/md103 -d agcount=64 -l size=2038m
 
 sudo mount -o logbsize=256k /dev/md127 /var/buildbuddy
 
