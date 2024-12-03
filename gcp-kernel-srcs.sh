57dc02a9e409e0b801d73ad74e88c4b35c618fc9
Jim Hollenbach <jim@buildbuddy.io>
2024-12-02 19:09:42 -0800

    scripts for building kernel

--- gcp-kernel-srcs.sh
+++ gcp-kernel-srcs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# fundamentally copied from https://wiki.ubuntu.com/Kernel/BuildYourOwnKernel
 set -o errexit -o nounset -o pipefail
 
 
@@ -7,5 +8,15 @@ sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
 sudo apt-get update
 
 sudo apt build-dep linux linux-image-unsigned-$(uname -r)
-sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
+sudo apt install fakeroot libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
 apt source linux-image-unsigned-$(uname -r)
+
+# afterward, in kernel root dir..
+# fakeroot debian/rules clean
+# fakeroot debian/rules binary
+
+# go back up a dir and install
+# cd ..
+# ls *.deb
+# sudo dpkg -i linux*VERSION*.deb
+# sudo reboot
