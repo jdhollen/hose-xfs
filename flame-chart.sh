8edb19d984b8ff280389558d0cd6adb31ed2c002
Jim Hollenbach <jim@buildbuddy.io>
2024-12-03 20:52:06 -0800

    remove unneeded env var

--- flame-chart.sh
+++ flame-chart.sh
@@ -4,7 +4,7 @@ set -o errexit -o nounset -o pipefail
 # fundamentally copied from https://www.brendangregg.com/FlameGraphs/cpuflamegraphs.html
 
 cd FlameGraph
-sudo DEBUGINFOD_URLS="https://debuginfod.ubuntu.com" perf record -F 99 -a -g --call-graph=dwarf -- sleep 60
+sudo perf record -F 99 -a -g --call-graph=dwarf -- sleep 60
 sudo chmod a+r perf.data
 sudo perf script --kallsyms=/proc/kallsyms | ./stackcollapse-perf.pl > out.perf-folded
 ./flamegraph.pl out.perf-folded > perf.svg
